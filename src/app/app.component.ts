dfb8985b3cfd43346577778f687f758a9284a80c
Prasanth1661993 <81809277+Prasanth1661993@users.noreply.github.com>
2021-04-09 17:53:52 +0530

    Successfully completed calling Treechart from Ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//  import  * as d3 from "d3";
+ import  * as d3 from "d3";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,19 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'ORG';
-  // data:any = null;
+  data:any = null;
   ngOnInit(){
-    // console.log("appComponent",this.data)
-    // d3.json(
-    //   "https://raw.githubusercontent.com/Prasanth1661993/MyRepo/master/Mock.json"
-    // ).then(data => {
-    //     this.data = data;
-    //     console.log("appComponent",this.data)
-    // });
+    console.log("appComponent",this.data)
+    d3.json(
+      "https://raw.githubusercontent.com/Prasanth1661993/MyRepo/master/Mock.json"
+    ).then(data => {
+        this.data = data;
+        console.log("appComponent",this.data)
+    });
     
   }
 
-  
+ 
 
   
 }
--- src/app/donutcharts/donutcharts.component.ts
+++ src/app/donutcharts/donutcharts.component.ts
@@ -4,10 +4,11 @@ import {  OnChanges,
   Input,
   ViewChild,
   ElementRef } from '@angular/core';
-// import * as d3 from "d3";
+//  import * as d3 from "d3";
 
-// declare function Chart(data:any) : any;
-
+    // declare function Chart() : any;
+  declare function myTest(data:any) : any;
+//  import TreeChart from "../../assets/JS/OrgC.js"
 // import  TreeChart from "d3-org-chart";
 
 @Component({
@@ -16,45 +17,52 @@ import {  OnChanges,
   styleUrls: ['./donutcharts.component.css']
 })
 export class DonutchartsComponent implements OnInit, OnChanges {
-  // @ViewChild("chartContainer") chartContainer: ElementRef;
-  // @Input() data: any[];
-  //  chart;
-  constructor() { }
+  @ViewChild("chartContainer") chartContainer: ElementRef;
+  @Input() data: any[];
+  chart;
+  getChartState : any;
+  constructor() {
   
-  ngOnInit(): void {
-    // console.log("Data",this.data)
   }
+
+  ngOnInit() {
+  
+  }
+
   ngAfterViewInit() {
-    // if (!this.chart) {
-  //     console.log("ngAfterViewInit",this.data)
-  //     // this.chart = Chart(this.data);
-  //     this.chart = new TreeChart;
-  //     console.log("Data", this.chart)
-  //   // }
-  //   this.updateChart();
-   }
+  
+    if (!this.chart) {
+     
+        this.chart = myTest(this.data);
+      // this.chart = new TreeChart;
+     
+    }
+    this.updateChart();
+  }
 
   ngOnChanges() {
     this.updateChart();
   }
+  onClick(){
+    myTest(this.data);
+  }
+
   updateChart() {
-    // console.log("updateChart",this.data)
-    // if (!this.data) {
-    //   return; 
-    // }
-    // if (!this.chart) {
-    //   return; 
-    // }
-    // this.chart
-    //   .container(this.chartContainer)
-    //   .data(this.data)
-    //   .svgWidth(500)
-    //   .initialZoom(0.4)
-    //   .onNodeClick(d => console.log(d + " node clicked"))
-    //   .render();
-   }
-  
-  
+    if (!this.data) {
+      return; 
+    }
+    if (!this.chart) {
+      return; 
+    }
+    this.chart
+      .container(this.chartContainer.nativeElement)
+      .data(this.data)
+      .svgWidth(500)
+      .initialZoom(0.6)
+      .onNodeClick(d => console.log(d + " node clicked"))
+      .render();
+
+  }
 
 }
 
